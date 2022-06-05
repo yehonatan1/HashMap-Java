b067971bfb27a7f3f6f541fa0c05352c29ed12c3
yehonatan1 <avitantan@gmail.com>
2022-06-05 14:37:01 -0700

    Test

--- LPHashTable.java
+++ LPHashTable.java
@@ -1,16 +1,23 @@
 import java.util.Random;
 
 public class LPHashTable extends OAHashTable {
-	
-	public LPHashTable(int m, long p) {
-		super(m);
-		// TODO Complete hash table constructor.
-	}
-	
-	@Override
-	public int Hash(long x, int i) {
-		// TODO implement hash function
-		return 0;
-	}
-	
+
+    int m;
+    long p;
+    ModHash modHash;
+
+    public LPHashTable(int m, long p) {
+        super(m);
+        this.m = m;
+        this.p = p;
+        modHash = ModHash.GetFunc(m, p);
+        // TODO Complete hash table constructor.
+    }
+
+    @Override
+    public int Hash(long x, int i) {
+        // TODO implement hash function
+        return (modHash.Hash(x) + i) % m;
+    }
+
 }
