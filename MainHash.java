1f244b4f8128d9ecacf845fc0ce55c1e6a0b09c6
yehonatan1 <avitantan@gmail.com>
2022-06-06 13:15:23 -0700

    Test

--- MainHash.java
+++ MainHash.java
@@ -1,9 +1,42 @@
+import java.util.Random;
+
 public class MainHash {
-    public static void main(String[] args) {
-//<<<<<<< HEAD
-        HashTableElement element = new HashTableElement.EmptyHashTableElement();
-        System.out.println(element.isEmpty());
-//=======
-//>>>>>>> origin/main
+    public static void main(String[] args) throws IHashTable.TableIsFullException, IHashTable.KeyAlreadyExistsException, IHashTable.KeyDoesntExistException {
+        int m = 10000019;
+        long p = 1000000007;
+        int n = Math.floorDiv(m, 2);
+        int array[] = new int[n];
+        Random random = new Random(2999);
+        int a, b;
+        OAHashTable table = new DoubleHashTable(m, p);
+        long startTime = System.currentTimeMillis();
+        for (int k = 0; k < 3; k++) {
+            for (int i = 0; i < n; i++) {
+                b = random.nextInt(100);
+                a = 100 * i + b;
+                array[i] = a;
+                table.Insert(new HashTableElement(a, 0));
+            }
+            for (int i = 0; i < n; i++) {
+                table.Delete(array[i]);
+            }
+        }
+
+        long estimatedTime = System.currentTimeMillis();
+        System.out.println(estimatedTime - startTime);
+        for (int k = 3; k < 6; k++) {
+            for (int i = 0; i < n; i++) {
+                b = random.nextInt(100);
+                a = 100 * i + b;
+                array[i] = a;
+                table.Insert(new HashTableElement(a, 0));
+            }
+            for (int i = 0; i < n; i++) {
+                table.Delete(array[i]);
+
+            }
+        }
+        long estimatedTime2 = System.currentTimeMillis();
+        System.out.println(estimatedTime2 - estimatedTime);
     }
 }
