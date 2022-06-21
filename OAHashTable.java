2c48c629257c0bd09d0e1236b697390d6a89a9c2
yehonatan1 <avitantan@gmail.com>
2022-06-21 16:00:54 -0700

    Test

--- OAHashTable.java
+++ OAHashTable.java
@@ -1,60 +1,69 @@
 
 public abstract class OAHashTable implements IHashTable {
-	
-	private final HashTableElement [] table;
-	private final int m;
-	
-	public OAHashTable(int m) {
-		this.table = new HashTableElement[m];
-		this.m = m;
-	}
-	
-	
-	@Override
-	public HashTableElement Find(long key) {
-		int index = findElementIndex(key);
-		if (index != -1)
-			return table[index];
-		return null;
-	}
-	
-	@Override
-	public void Insert(HashTableElement hte) throws TableIsFullException,KeyAlreadyExistsException {
-		for (int i = 0; i < m; i++){
-			int index = Hash(hte.GetKey(), i);
-			if (table[index] == null || table[index].isEmpty()){
-				table[index] = hte;
-				return;
-			} else if (table[index].GetKey() == hte.GetKey())
-				throw new KeyAlreadyExistsException(hte);
-		}
-		throw new TableIsFullException(hte);
-	}
-	
-	@Override
-	public void Delete(long key) throws KeyDoesntExistException {
-		int index = findElementIndex(key);
-		if (index == -1)
-			throw new KeyDoesntExistException(key);
-		table[index] = new HashTableElement.EmptyHashTableElement();
-	}
-	
-	/**
-	 * 
-	 * @param x - the key to hash
-	 * @param i - the index in the probing sequence
-	 * @return the index into the hash table to place the key x
-	 */
-	public abstract int Hash(long x, int i);
 
-	private int findElementIndex(long key){
-		for (int i = 0; i < m; i++){
-			int index = Hash(key, i);
-			if (!table[index].isEmpty() && table[index].GetKey() == key)
-				return index;
-			else if (table[index] == null)
-				return -1;
-		}
-		return -1;
-	}
+    private final HashTableElement[] table;
+    private final int m;
+
+    public OAHashTable(int m) {
+        this.table = new HashTableElement[m];
+        this.m = m;
+    }
+
+
+    @Override
+    public HashTableElement Find(long key) {
+        int index = findElementIndex(key);
+        if (index != -1)
+            return table[index];
+        return null;
+    }
+
+    @Override
+    public void Insert(HashTableElement hte) throws TableIsFullException, KeyAlreadyExistsException {
+        for (int i = 0; i < m; i++) {
+            int index = Hash(hte.GetKey(), i);
+            if (table[index] == null) {
+                table[index] = hte;
+                return;
+            } else if (table[index].isEmpty()) {
+                table[index] = hte;
+                for (int j = i + 1; j < m; j++) {
+                    index = Hash(hte.GetKey(), i);
+                    if (table[index] == null)
+                        return;
+                    else if (!table[index].isEmpty() && table[index].GetKey() == hte.GetKey())
+                        throw new KeyAlreadyExistsException(hte);
+                }
+                return;
+            } else if (table[index].GetKey() == hte.GetKey())
+                throw new KeyAlreadyExistsException(hte);
+        }
+        throw new TableIsFullException(hte);
+    }
+
+    @Override
+    public void Delete(long key) throws KeyDoesntExistException {
+        int index = findElementIndex(key);
+        if (index == -1)
+            throw new KeyDoesntExistException(key);
+        table[index] = new HashTableElement.EmptyHashTableElement();
+    }
+
+    /**
+     * @param x - the key to hash
+     * @param i - the index in the probing sequence
+     * @return the index into the hash table to place the key x
+     */
+    public abstract int Hash(long x, int i);
+
+    private int findElementIndex(long key) {
+        for (int i = 0; i < m; i++) {
+            int index = Hash(key, i);
+            if (!table[index].isEmpty() && table[index].GetKey() == key)
+                return index;
+            else if (table[index] == null)
+                return -1;
+        }
+        return -1;
+    }
 }
